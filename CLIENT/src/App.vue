1112b5c0b1135f1706f29c4f8d40684a305bfca1
uknowsj <uknowsj@gmail.com>
2021-03-05 10:02:12 +0900

    @vue/cli 설치 및 테스트

---
+++ CLIENT/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>
+      테스트 페이지
+    </h1>
+  </div>
+</template>
+
+<script scoped>
+  export default {
+    name: "app",
+    components: {}
+  };
+</script>
+
+<style scoped>
+  div h1{
+    color:#03a9f4;
+  }
+</style>
